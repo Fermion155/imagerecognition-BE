728401b3c700dad7d31f764904534e5d9146ef5b
pdrabuzinskis@gmail.com <pdrabuzinskis@gmail.com>
2024-11-18 21:28:31 +0200

    Initial commit

---
+++ src/main/java/com/fashiontruck/imagerecognition_BE/ImagerecognitionBeApplication.java
@@ -0,0 +1,13 @@
+package com.fashiontruck.imagerecognition_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagerecognitionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagerecognitionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fashiontruck/imagerecognition_BE/ImagerecognitionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashiontruck.imagerecognition_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagerecognitionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
